9eba716880bb902c0a8fe786ecbc5f249c8fe81e
ivang <redial@abv.bg>
2023-12-25 22:09:42 +0200

    Filters under search bar

--- race-rating-ui/src/app/racelist/racelist.component.ts
+++ race-rating-ui/src/app/racelist/racelist.component.ts
@@ -4,19 +4,59 @@ import {NgForOf} from "@angular/common";
 import {RacelistCardComponent} from "./racelist-card/racelist-card.component";
 import {RaceListModel} from "./RaceListModel";
 import {SearchBarComponent} from "./search-bar/search-bar.component";
+import {MatSelectModule} from "@angular/material/select";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {ThemePalette} from "@angular/material/core";
+import {FormsModule} from "@angular/forms";
 
+export interface Terrain {
+  name: string;
+  completed: boolean;
+  color: ThemePalette;
+  subtasks?: Terrain[];
+}
 @Component({
   selector: 'app-racelist',
   standalone: true,
   imports: [
     NgForOf,
     RacelistCardComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    MatSelectModule,
+    MatDividerModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   templateUrl: './racelist.component.html',
   styleUrl: './racelist.component.scss'
 })
 export class RacelistComponent implements OnInit{
+
+  minDistance: number = 0;
+  maxDistance: number = 200;
+  minElevation: number = 0;
+  maxElevation: number = 5000;
+  selectedTerrain: string = 'all';
+
+  roadTerrain: Terrain = {
+    name: 'Road',
+    completed: false,
+    color: 'primary'
+  }
+
+  offRoadTerrain: Terrain = {
+    name: 'Off-road',
+    completed: false,
+    color: 'primary',
+    subtasks: [
+      {name: 'Trail', completed: false, color: 'primary'},
+      {name: 'Technical trail', completed: false, color: 'primary'},
+      {name: 'Big mountain', completed: false, color: 'accent'},
+    ],
+  };
   allRaces: RaceListModel[] = [
     {id: 1, name: 'Balkan Ultra',  distance: 78, elevation: 6700, rating: 5, logoUrl: 'https://balkanultra.s3.eu-central-1.amazonaws.com/images/home/why.jpg'},
     {id: 2, name: 'Tryavna Ultra',  distance: 150, elevation: 6684, rating: 4, logoUrl: 'https://i.ytimg.com/vi/BgqGre6bkBg/sddefault.jpg'},
@@ -35,5 +75,27 @@ export class RacelistComponent implements OnInit{
     // this.raceService.fetchData().subscribe({
     //   next: (data) => this.movies = data
     // })
+  }
+
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete = this.offRoadTerrain.subtasks != null && this.offRoadTerrain.subtasks.every(t => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.offRoadTerrain.subtasks == null) {
+      return false;
+    }
+    return this.offRoadTerrain.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.offRoadTerrain.subtasks == null) {
+      return;
+    }
+    this.offRoadTerrain.subtasks.forEach(t => (t.completed = completed));
   }
 }
