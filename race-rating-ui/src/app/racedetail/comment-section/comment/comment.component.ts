018f93295e3e32e22e403a045c0c13984bb02e05
ivang <redial@abv.bg>
2023-12-28 20:06:35 +0200

    Styling the comments

--- race-rating-ui/src/app/racedetail/comment-section/comment/comment.component.ts
+++ race-rating-ui/src/app/racedetail/comment-section/comment/comment.component.ts
@@ -1,11 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {RaceListModel} from "../../../racelist/RaceListModel";
 import {RaceComment} from "./race-comment.model";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
--- race-rating-ui/src/app/racedetail/comment-section/dummyComments.ts
+++ race-rating-ui/src/app/racedetail/comment-section/dummyComments.ts
@@ -5,21 +5,21 @@ export const dummyComments: RaceComment[] = [
     id: 1,
     author: 'John Doe',
     createdOn: new Date('2021-01-01'),
-    authorAvatarUrl: 'https://example.com/avatar1.jpg',
+    authorAvatarUrl: 'https://lh3.googleusercontent.com/a/AATXAJynIOfHLyXgk7y9v_GWsXkFeHV0tHGMbzXsACwx=s96-c64',
     commentText: 'This was a great race! Really enjoyed the track and the organization.'
   },
   {
     id: 2,
     author: 'Jane Smith',
     createdOn: new Date('2021-02-15'),
-    authorAvatarUrl: 'https://example.com/avatar2.jpg',
+    authorAvatarUrl: 'https://lh3.googleusercontent.com/a/AATXAJynIOfHLyXgk7y9v_GWsXkFeHV0tHGMbzXsACwx=s96-c64',
     commentText: 'Beautiful scenery and a well-organized event. Looking forward to the next one!'
   },
   {
     id: 3,
     author: 'Alice Johnson',
     createdOn: new Date('2021-03-10'),
-    authorAvatarUrl: 'https://example.com/avatar3.jpg',
+    authorAvatarUrl: 'https://lh3.googleusercontent.com/a/AATXAJynIOfHLyXgk7y9v_GWsXkFeHV0tHGMbzXsACwx=s96-c64',
     commentText: 'Challenging course, but very rewarding. The support from the staff was fantastic.'
   },
   {
@@ -33,7 +33,7 @@ export const dummyComments: RaceComment[] = [
     id: 5,
     author: 'Charlie Davis',
     createdOn: new Date('2021-05-20'),
-    authorAvatarUrl: 'https://example.com/avatar5.jpg',
+    authorAvatarUrl: 'https://lh3.googleusercontent.com/a/AATXAJynIOfHLyXgk7y9v_GWsXkFeHV0tHGMbzXsACwx=s96-c64',
     commentText: 'Loved every moment! The race was exhilarating and the organization was top-notch.'
   }
   // ... add more comments as needed
