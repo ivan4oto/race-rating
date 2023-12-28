7af3d7afe37c403aafb2c42f5e9aa88c7ab007af
ivang <redial@abv.bg>
2023-12-28 10:20:39 +0200

    Routing racedetail component

--- race-rating-ui/src/app/app.routes.ts
+++ race-rating-ui/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {RacelistComponent} from "./racelist/racelist.component";
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {OAuth2RedirectHandlerComponent} from "./auth/oauth2-redirect-handler/oauth2-redirect-handler.component";
+import {RacedetailComponent} from "./racedetail/racedetail.component";
 
 export const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'oauth2/redirect', component: OAuth2RedirectHandlerComponent},
   {path: 'race/all', component: RacelistComponent},
+  {path: 'race/:id', component: RacedetailComponent},
   {path: 'about', component: AboutComponent}
 ];
