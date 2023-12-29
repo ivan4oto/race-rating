d441fbb143d6c56ee136ae007c3aa9dbeffe15e8
ivang <redial@abv.bg>
2023-12-29 10:16:45 +0200

    add comment form

--- race-rating-ui/src/app/racedetail/comment-section/comment-section.component.ts
+++ race-rating-ui/src/app/racedetail/comment-section/comment-section.component.ts
@@ -3,13 +3,15 @@ import {RaceComment} from "./comment/race-comment.model";
 import {dummyComments} from "./dummyComments";
 import {CommentComponent} from "./comment/comment.component";
 import {NgForOf} from "@angular/common";
+import {CommentFormComponent} from "./comment-form/comment-form.component";
 
 @Component({
   selector: 'app-comment-section',
   standalone: true,
   imports: [
     CommentComponent,
-    NgForOf
+    NgForOf,
+    CommentFormComponent
   ],
   templateUrl: './comment-section.component.html',
   styleUrl: './comment-section.component.scss'
