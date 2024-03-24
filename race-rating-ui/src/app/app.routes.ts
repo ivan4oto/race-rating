00e9bb132f0dd6176060a8e43af4c4d5ed820d47
Ivan Gochev <redial@abv.bg>
2024-03-24 22:40:51 +0200

    Login component page

--- race-rating-ui/src/app/app.routes.ts
+++ race-rating-ui/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import {RacelistComponent} from "./racelist/racelist.component";
-import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {OAuth2RedirectHandlerComponent} from "./auth/oauth2-redirect-handler/oauth2-redirect-handler.component";
 import {RacedetailComponent} from "./racedetail/racedetail.component";
 import {CreateRaceComponent} from "./create-race/create-race.component";
 import {AuthGuard} from "./auth/guard/auth.guard";
 import {EditRaceComponent} from "./edit-race/edit-race.component";
+import {LoginComponent} from "./auth/login/login.component";
 
 export const routes: Routes = [
   {path: '', pathMatch: 'full', component: RacelistComponent},
@@ -15,5 +15,6 @@ export const routes: Routes = [
   {path: 'race/:id', component: RacedetailComponent},
   {path: 'race/:id/edit', component: EditRaceComponent}, // canActivate: [AuthGuard]},
   {path: 'create', component: CreateRaceComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
 ];
---
+++ race-rating-ui/src/app/auth/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  hide = true;
+  signUp() {
+    // sign-up logic
+  }
+}
