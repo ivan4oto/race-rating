5f03716ec852b428d87a8c294edceec7b2bcb7d2
Ivan Gochev <redial@abv.bg>
2024-03-24 17:51:55 +0200

    Removing redundant home and signup components

--- race-rating-ui/src/app/app.component.ts
+++ race-rating-ui/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {SignupComponent} from "./auth/signup/signup.component";
 import {NavbarComponent} from "./navbar/navbar.component";
-import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SignupComponent, NavbarComponent, HomeComponent, AboutComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
