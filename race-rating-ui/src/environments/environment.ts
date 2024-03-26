eb56d6068a2ea8f0e094f5946c3a60633c212b06
Ivan Gochev <redial@abv.bg>
2024-03-26 09:11:36 +0200

    Updating urls

--- race-rating-ui/src/environments/environment.ts
+++ race-rating-ui/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://race-rating-production.up.railway.app/',
+  apiUrl: 'https://race-rating-production.up.railway.app/',
   oauth2AuthorizationBaseUrl: 'https://race-rating-production.up.railway.app/oauth2/authorization/google',
   redirectUri: 'https://racerating.bg/oauth2/redirect'
 };
