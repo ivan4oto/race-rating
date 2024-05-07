321486315bc6bc1242dffa390b595a7554f98457
Ivan Gochev <redial@abv.bg>
2024-05-07 19:25:01 +0300

    Average ratings impl logic (#17)
    * avg rating range labels
    
    * new attribute ratingsCount to Race
    
    * flyway sql init
    
    * jpb settings whatever that is
    
    * update dummy data
    
    * Unused return
    
    * Avg rating calculation logic

--- race-rating-ui/src/app/racedetail/avg-rating-widget/avg-rating-widget.component.ts
+++ race-rating-ui/src/app/racedetail/avg-rating-widget/avg-rating-widget.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-avg-rating-widget',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './avg-rating-widget.component.html',
   styleUrl: './avg-rating-widget.component.scss'
 })
