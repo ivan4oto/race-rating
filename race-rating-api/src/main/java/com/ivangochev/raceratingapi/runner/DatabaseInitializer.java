0964a2ae240b0878334d2fe80bac1a48e7478fce
Ivan Gochev <redial@abv.bg>
2024-05-07 20:08:50 +0300

    Disable default db initialization

--- race-rating-api/src/main/java/com/ivangochev/raceratingapi/runner/DatabaseInitializer.java
+++ race-rating-api/src/main/java/com/ivangochev/raceratingapi/runner/DatabaseInitializer.java
@@ -41,29 +41,29 @@ public class DatabaseInitializer implements CommandLineRunner {
         List<Rating> ratings;
         List<RaceComment> comments;
 
-        try {
-            String usersJson = ResourceFileReader.readJsonFileFromClasspath("users.json");
-            List<User> users = jsonUtils.fromJsonToUsers(usersJson);
-            users.forEach(user -> {
-                user.setPassword(passwordEncoder.encode(user.getPassword()));
-                userService.saveUser(user);
-            });
-
-            String racesJson = ResourceFileReader.readJsonFileFromClasspath("races.json");
-            races = jsonUtils.fromJsonToRaces(racesJson);
-            raceService.saveAllRaces(races);
-
-            String ratingsJson = ResourceFileReader.readJsonFileFromClasspath("ratings.json");
-            ratings = jsonUtils.fromJsonToRatings(ratingsJson);
-            ratingService.saveAllRatings(ratings);
-
-            String commentsJson = ResourceFileReader.readJsonFileFromClasspath("comments.json");
-            comments = jsonUtils.fromJsonToComments(commentsJson);
-            commentRepository.saveAll(comments);
-        } catch (IOException e) {
-            log.error("Unable to load database objects from json!");
-            e.printStackTrace();
-        }
+//        try {
+//            String usersJson = ResourceFileReader.readJsonFileFromClasspath("users.json");
+//            List<User> users = jsonUtils.fromJsonToUsers(usersJson);
+//            users.forEach(user -> {
+//                user.setPassword(passwordEncoder.encode(user.getPassword()));
+//                userService.saveUser(user);
+//            });
+//
+//            String racesJson = ResourceFileReader.readJsonFileFromClasspath("races.json");
+//            races = jsonUtils.fromJsonToRaces(racesJson);
+//            raceService.saveAllRaces(races);
+//
+//            String ratingsJson = ResourceFileReader.readJsonFileFromClasspath("ratings.json");
+//            ratings = jsonUtils.fromJsonToRatings(ratingsJson);
+//            ratingService.saveAllRatings(ratings);
+//
+//            String commentsJson = ResourceFileReader.readJsonFileFromClasspath("comments.json");
+//            comments = jsonUtils.fromJsonToComments(commentsJson);
+//            commentRepository.saveAll(comments);
+//        } catch (IOException e) {
+//            log.error("Unable to load database objects from json!");
+//            e.printStackTrace();
+//        }
 
         log.info("Database initialized");
     }
