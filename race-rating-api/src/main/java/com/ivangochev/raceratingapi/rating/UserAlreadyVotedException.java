c18fd5311826c11cf457cca04ec3ae34aab650ff
ivang <redial@abv.bg>
2024-09-19 20:04:18 +0300

    Custom exception with more logging

--- race-rating-api/src/main/java/com/ivangochev/raceratingapi/rating/UserAlreadyVotedException.java
+++ race-rating-api/src/main/java/com/ivangochev/raceratingapi/rating/UserAlreadyVotedException.java
@@ -1,11 +1,12 @@
 package com.ivangochev.raceratingapi.rating;
 
+import com.ivangochev.raceratingapi.utils.BaseCustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class UserAlreadyVotedException extends RuntimeException{
+public class UserAlreadyVotedException extends BaseCustomException {
     public UserAlreadyVotedException(String message) {
-        super(message);
+        super(message, HttpStatus.CONFLICT);
     }
 }
