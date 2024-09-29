5760ec7a0a6c0d12e414ceb0059b2b65561674c9
ivang <redial@abv.bg>
2024-09-29 13:40:31 +0300

    Add ng toastr

--- race-rating-ui/src/app/app.config.ts
+++ race-rating-ui/src/app/app.config.ts
@@ -1,15 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {routes} from './app.routes';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {AuthInterceptor} from "./auth/AuthInterceptor";
+import {provideToastr} from "ngx-toastr";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-full-width'
+    }),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
