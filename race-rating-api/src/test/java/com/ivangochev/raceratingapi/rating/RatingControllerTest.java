34c742186ad5fa4d11b2ff61a4c1b1079d57b30a
Ivan Gochev <redial@abv.bg>
2024-10-24 09:00:35 +0300

    Testing auth secured controller

--- race-rating-api/src/test/java/com/ivangochev/raceratingapi/rating/RatingControllerTest.java
+++ race-rating-api/src/test/java/com/ivangochev/raceratingapi/rating/RatingControllerTest.java
@@ -1,8 +1,12 @@
 package com.ivangochev.raceratingapi.rating;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ivangochev.raceratingapi.config.CustomTestConfig;
 import com.ivangochev.raceratingapi.race.Race;
 import com.ivangochev.raceratingapi.race.RaceRepository;
 import com.ivangochev.raceratingapi.security.TokenAuthenticationFilter;
+import com.ivangochev.raceratingapi.security.oauth2.OAuth2Provider;
+import com.ivangochev.raceratingapi.user.User;
 import com.ivangochev.raceratingapi.user.UserService;
 import com.ivangochev.raceratingapi.user.mapper.UserMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,19 +15,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = RatingController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@Import(CustomTestConfig.class)
 class RatingControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -37,6 +48,8 @@ class RatingControllerTest {
     private UserMapper userMapper;
     @MockBean
     private TokenAuthenticationFilter tokenAuthenticationFilter;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
@@ -79,5 +92,53 @@ class RatingControllerTest {
                 .andExpect(status().isNotFound());
         verify(raceRepository, times(1)).findById(raceId);
         verify(ratingService, never()).findByRace(any());
+    }
+    @Test
+    @WithUserDetails(setupBefore = TestExecutionEvent.TEST_EXECUTION, value = "ivan", userDetailsServiceBeanName = "userDetailsServiceMock")
+    public void testCreateRating_Success() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("ivan");
+        user.setPassword("password");
+        user.setEmail("ivan@abv.bg");
+        user.setName("ivan");
+        user.setCommentedForRaces(List.of());
+        user.setRole("ADMIN");
+        user.setImageUrl("kdowa");
+        user.setProvider(OAuth2Provider.GOOGLE);
+        user.setProviderId("3");
+
+        RatingDto ratingDto = new RatingDto();
+        ratingDto.setId(1L);
+        ratingDto.setValueScore(3);
+        ratingDto.setLocationScore(5);
+        ratingDto.setAuthorId(1L);
+
+        Rating savedRating = new Rating();
+        savedRating.setId(1L);
+        savedRating.setValueScore(3);
+        savedRating.setLocationScore(5);
+        savedRating.setOrganizationScore(5);
+        savedRating.setTraceScore(5);
+        savedRating.setVibeScore(5);
+        savedRating.setAuthor(user);
+        savedRating.setCreatedAt(new Date());
+        savedRating.setAuthor(user);
+
+        when(userService.validateAndGetUserByUsername("ivan")).thenReturn(user);
+        when(ratingService.saveRating(any(RatingDto.class), eq(user))).thenReturn(savedRating);
+
+        // When & Then
+        mockMvc.perform(post("/api/ratings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(ratingDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.valueScore").value(ratingDto.getValueScore()))
+                .andExpect(jsonPath("$.locationScore").value(ratingDto.getLocationScore()))
+                .andExpect(jsonPath("$.author.id").value(ratingDto.getAuthorId()));
+
+        verify(userService, times(1)).validateAndGetUserByUsername("ivan");
+        verify(ratingService, times(1)).saveRating(any(RatingDto.class), eq(user));
     }
 }
