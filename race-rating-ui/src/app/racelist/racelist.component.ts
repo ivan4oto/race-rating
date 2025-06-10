d4526426a8032d28f8f12225456cd10c595dd17c
Ivan Gochev <redial@abv.bg>
2025-06-10 15:43:25 +0300

    Use query params to remember pagination

--- race-rating-ui/src/app/racelist/racelist.component.ts
+++ race-rating-ui/src/app/racelist/racelist.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {RaceService} from "./race.service";
 import {NgForOf} from "@angular/common";
 import {RaceListModel} from "./race-list.model";
@@ -9,10 +9,11 @@ import {MatSliderModule} from "@angular/material/slider";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {FormsModule} from "@angular/forms";
 import Fuse from "fuse.js";
-import {MatPaginatorModule, PageEvent} from "@angular/material/paginator";
-import {RouterLink} from "@angular/router";
+import {MatPaginator, MatPaginatorModule, PageEvent} from "@angular/material/paginator";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {RaceListCustomCardComponent} from "./race-list-custom-card/race-list-custom-card.component";
 import {MatButtonModule} from "@angular/material/button";
+import {combineLatest, map} from "rxjs";
 
 @Component({
   selector: 'app-racelist',
@@ -34,11 +35,13 @@ import {MatButtonModule} from "@angular/material/button";
   styleUrl: './racelist.component.scss'
 })
 export class RacelistComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   allRaces: RaceListModel[] = [];
   filteredRaces: RaceListModel[] = [];
   currentRaces: RaceListModel[] = [];
-  pageSize = 5;
-  pageSizeOptions: number[] = [5, 10, 25, 100];
+  private pageFromQueryParams: number = 0;
+  pageSize = 10;
+  pageSizeOptions: number[] = [10, 25, 50];
   fuse!: Fuse<RaceListModel>;
   fuseOptions = {
     keys: ["name"],
@@ -50,19 +53,34 @@ export class RacelistComponent implements OnInit {
 
   constructor(
     private raceService: RaceService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
   }
 
-
   ngOnInit() {
-    this.raceService.fetchAllRaces().subscribe({
-      next: (data: RaceListModel[]) => {
+    combineLatest([
+      this.raceService.fetchAllRaces(),
+      this.route.queryParams
+    ])
+      .pipe(
+        map(([data, params]) => {
+          const page = +params['page'] || 1;
+          this.pageFromQueryParams = page - 1;
+          return { data, page };
+        })
+      )
+      .subscribe(({ data, page }) => {
         this.allRaces = data;
-        this.filteredRaces = this.allRaces;
-        this.updateCurrentRaces(0);
+        this.filteredRaces = [...this.allRaces]; // optional: clone to avoid side effects
+        this.updateCurrentRaces(page - 1);
         this.fuse = new Fuse(this.allRaces, this.fuseOptions);
-      }
-    })
+        Promise.resolve().then(() => {
+          if (this.paginator) {
+            this.paginator.pageIndex = page - 1;
+          }
+        });
+      });
   }
 
   onSearchTermChange(searchTerm: string) {
@@ -75,8 +93,14 @@ export class RacelistComponent implements OnInit {
     const end = start + this.pageSize;
     this.currentRaces = this.filteredRaces.slice(start, end);
   }
+
   pageEvent($event: PageEvent) {
     this.pageSize = $event.pageSize;
     this.updateCurrentRaces($event.pageIndex);
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page: $event.pageIndex + 1 },
+      queryParamsHandling: 'merge',
+    });
   }
 }
