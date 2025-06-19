ebee3521233b7604f8916a330d04505e82547c4b
Ivan Gochev <redial@abv.bg>
2025-06-19 17:55:40 +0300

    Fix oauth login issue cookies not stored before user details call

--- race-rating-ui/src/app/auth/oauth2-redirect-handler/oauth2-redirect-handler.component.ts
+++ race-rating-ui/src/app/auth/oauth2-redirect-handler/oauth2-redirect-handler.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-oauth2-redirect-handler',
@@ -13,22 +12,9 @@ export class  OAuth2RedirectHandlerComponent implements OnInit{
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService
   ) {
   }
   ngOnInit() {
-      this.authService.storeUserInformation().subscribe(
-        {
-          next: (response) => {
-            if (response.body) {
-              console.log('Success login.')
-            }
-          },
-          error: (err) => {
-            console.log(err);
-          }
-        }
-      );
       this.router.navigate(['/']); // Redirect to home
   }
 
