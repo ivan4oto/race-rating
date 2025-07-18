d489afec565c307f45432eaef1458b143d57c489
Ivan Gochev <redial@abv.bg>
2025-07-18 17:23:29 +0300

    avatar migration

--- race-rating-api/src/main/java/com/ivangochev/raceratingapi/avatar/AvatarService.java
+++ race-rating-api/src/main/java/com/ivangochev/raceratingapi/avatar/AvatarService.java
@@ -4,6 +4,7 @@ import com.ivangochev.raceratingapi.config.AwsProperties;
 import com.ivangochev.raceratingapi.user.User;
 import com.ivangochev.raceratingapi.utils.AvatarGenerator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -14,12 +15,15 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j
 public class AvatarService {
     private final S3Client s3Client;
     private final AwsProperties awsProperties;
+    @Value("${aws.s3.root-folder}")
+    private String s3RootFolder;
 
 
     public AvatarService(S3Client s3Client, AwsProperties awsProperties) {
@@ -32,16 +36,18 @@ public class AvatarService {
     }
 
     private String getAvatarKey(Long userId) {
-        return "resources/avatars/" + userId + ".png";
+        return s3RootFolder + "/avatars/" + userId + "/avatar.png";
     }
 
     @Async
-    public void generateAndUploadAvatar(User user) {
+    public CompletableFuture<Void> generateAndUploadAvatar(User user) {
         try {
             BufferedImage avatar = AvatarGenerator.generateAvatar(user.getUsername(), 128);
             uploadAvatarToS3(avatar, user.getId()); // Save as userID.png
+            return CompletableFuture.completedFuture(null);
         } catch (Exception e) {
             log.error("Error while generating/uploading avatar", e);
+            return CompletableFuture.failedFuture(e);
         }
     }
 
